6b4f2bc89e7543be8ab1692e1fa4a5d89aca35d5
TheHeadyTV <58431566+TheHeadyTV@users.noreply.github.com>
2020-04-24 05:25:54 +0200

    Adding Paths in installer.sh

--- installer.sh
+++ installer.sh
@@ -5,22 +5,25 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+# Variable for PATHS
+piholeDir=/var/www/html/admin
+
 while true; do
     read -p "Create a backup of the current files? (Y/n): " yn
     case $yn in
         [Yy]|""* ) 
 				# Backup of all changed files...
-				cp -f admin/style/vendor/AdminLTE.min.css admin/style/vendor/AdminLTE.min.css.orig
-				cp -f admin/style/vendor/skin-blue.min.css admin/style/vendor/skin-blue.min.css.orig
-				cp -f admin/scripts/pi-hole/js/index.js admin/scripts/pi-hole/js/index.js.orig
-				cp -f admin/scripts/pi-hole/js/auditlog.js admin/scripts/pi-hole/js/auditlog.js.orig
-				cp -f admin/scripts/pi-hole/js/network.js admin/scripts/pi-hole/js/network.js.orig
-				cp -f admin/scripts/pi-hole/js/queries.js admin/scripts/pi-hole/js/queries.js.orig
-				cp -f admin/scripts/pi-hole/js/db_queries.js admin/scripts/pi-hole/js/db_queries.js.orig
-				cp -f admin/scripts/pi-hole/php/queryads.php admin/scripts/pi-hole/php/queryads.php.orig
-				cp -f admin/index.php admin/index.php.orig
-				cp -f admin/network.php admin/network.php.orig
-				cp -f admin/db_queries.php admin/db_queries.php.orig
+				cp -f $piholeDir/style/vendor/AdminLTE.min.css $piholeDir/style/vendor/AdminLTE.min.css.orig
+				cp -f $piholeDir/style/vendor/skin-blue.min.css $piholeDir/style/vendor/skin-blue.min.css.orig
+				cp -f $piholeDir/scripts/pi-hole/js/index.js $piholeDir/scripts/pi-hole/js/index.js.orig
+				cp -f $piholeDir/scripts/pi-hole/js/auditlog.js $piholeDir/scripts/pi-hole/js/auditlog.js.orig
+				cp -f $piholeDir/scripts/pi-hole/js/network.js $piholeDir/scripts/pi-hole/js/network.js.orig
+				cp -f $piholeDir/scripts/pi-hole/js/queries.js $piholeDir/scripts/pi-hole/js/queries.js.orig
+				cp -f $piholeDir/scripts/pi-hole/js/db_queries.js $piholeDir/scripts/pi-hole/js/db_queries.js.orig
+				cp -f $piholeDir/scripts/pi-hole/php/queryads.php $piholeDir/scripts/pi-hole/php/queryads.php.orig
+				cp -f $piholeDir/index.php $piholeDir/index.php.orig
+				cp -f $piholeDir/network.php $piholeDir/network.php.orig
+				cp -f $piholeDir/db_queries.php $piholeDir/db_queries.php.orig
 				echo "Backup completed!"
 				break;;
         [Nn]* ) break;;
@@ -33,30 +36,30 @@ while true; do
         [Yy]|""* ) 
 				git clone https://github.com/TheHeadyTV/pihole.git temp
 				# Deleting all existing files...
-				rm -f admin/style/vendor/AdminLTE.min.css
-				rm -f admin/style/vendor/skin-blue.min.case
-				rm -f admin/scripts/pi-hole/js/index.js
-				rm -f admin/scripts/pi-hole/js/auditlog.js
-				rm -f admin/scripts/pi-hole/js/network.js
-				rm -f admin/scripts/pi-hole/js/queries.js
-				rm -f admin/scripts/pi-hole/js/db_queries.js
-				rm -f admin/scripts/pi-hole/php/queryads.php
-				rm -f admin/index.php
-				rm -f admin/network.php
-				rm -f admin/db_queries.php
+				rm -f $piholeDir/style/vendor/AdminLTE.min.css
+				rm -f $piholeDir/style/vendor/skin-blue.min.case
+				rm -f $piholeDir/scripts/pi-hole/js/index.js
+				rm -f $piholeDir/scripts/pi-hole/js/auditlog.js
+				rm -f $piholeDir/scripts/pi-hole/js/network.js
+				rm -f $piholeDir/scripts/pi-hole/js/queries.js
+				rm -f $piholeDir/scripts/pi-hole/js/db_queries.js
+				rm -f $piholeDir/scripts/pi-hole/php/queryads.php
+				rm -f $piholeDir/index.php
+				rm -f $piholeDir/network.php
+				rm -f $piholeDir/db_queries.php
 				
 				# Copying new files...
-				cp temp/style/vendor/AdminLTE.min.css admin/style/vendor/AdminLTE.min.css
-				cp temp/style/vendor/skin-blue.min.css admin/style/vendor/skin-blue.min.css
-				cp temp/scripts/pi-hole/js/index.js admin/scripts/pi-hole/js/index.js
-				cp temp/scripts/pi-hole/js/auditlog.js admin/scripts/pi-hole/js/auditlog.js
-				cp temp/scripts/pi-hole/js/network.js admin/scripts/pi-hole/js/network.js
-				cp temp/scripts/pi-hole/js/queries.js admin/scripts/pi-hole/js/queries.js
-				cp temp/scripts/pi-hole/js/db_queries.js admin/scripts/pi-hole/js/db_queries.js
-				cp temp/scripts/pi-hole/php/queryads.php admin/scripts/pi-hole/php/queryads.php
-				cp temp/index.php admin/index.php
-				cp temp/network.php admin/network.php
-				cp temp/db_queries.php admin/db_queries.php
+				cp temp/style/vendor/AdminLTE.min.css $piholeDir/style/vendor/AdminLTE.min.css
+				cp temp/style/vendor/skin-blue.min.css $piholeDir/style/vendor/skin-blue.min.css
+				cp temp/scripts/pi-hole/js/index.js $piholeDir/scripts/pi-hole/js/index.js
+				cp temp/scripts/pi-hole/js/auditlog.js $piholeDir/scripts/pi-hole/js/auditlog.js
+				cp temp/scripts/pi-hole/js/network.js $piholeDir/scripts/pi-hole/js/network.js
+				cp temp/scripts/pi-hole/js/queries.js $piholeDir/scripts/pi-hole/js/queries.js
+				cp temp/scripts/pi-hole/js/db_queries.js $piholeDir/scripts/pi-hole/js/db_queries.js
+				cp temp/scripts/pi-hole/php/queryads.php $piholeDir/scripts/pi-hole/php/queryads.php
+				cp temp/index.php $piholeDir/index.php
+				cp temp/network.php $piholeDir/network.php
+				cp temp/db_queries.php $piholeDir/db_queries.php
 					
 				rm -rf temp/
 				break;;
